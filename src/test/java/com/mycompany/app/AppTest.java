c8bca6ec56999839eea702bcaa2c6e2c41ccacbc
mertilovski <mertkaya@etu.edu.tr>
2019-02-02 22:35:55 +0300

    AppTest trial 1

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,43 +1,61 @@
 package com.mycompany.app;
-import junit.framework.TestCase;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Map;
-public class AppTest extends TestCase{
 
-  public void testStandartDeviationMethodMixCases(){
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-    ArrayList<Integer> input1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
-    ArrayList<Integer> input2 = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1,1,1,1,1,1));
-    ArrayList<Integer> input3 = new ArrayList<Integer>(Arrays.asList(1,1,2,4,3));
-    ArrayList<Integer> input4 = new ArrayList<Integer>(Arrays.asList(5,5,5,5,5));
-    String str = App.standartDeviation(input1, input2, input3, input4);
-    assertTrue((str.equals("2nd list.Its standart deviation is : 1.0")));
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
-  }
-  public void testWithNull() {
-    ArrayList<Integer> input1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
-    ArrayList<Integer> input2 = new ArrayList<Integer>(Arrays.asList(1,1,2,4,1));
-    ArrayList<Integer> input3 = new ArrayList<Integer>(Arrays.asList(1,1,2,4,3));
-    ArrayList<Integer> input4 = new ArrayList<Integer>(Arrays.asList(5,5,5,5,5));
-    String str = App.add(input1, input2, null, input4);
-    assertTrue((str.equals("input4")));
-  }
-  public void testWithAllNull() {
-    String str = App.add(null, null, null, null);
-    assertTrue((str.equals("")));
-  }
-  public void testEqual() {
-    ArrayList<Integer> input1 = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1));
-    ArrayList<Integer> input2 = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1));
-    ArrayList<Integer> input3 = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1));
-    ArrayList<Integer> input4 = new ArrayList<Integer>(Arrays.asList(5));
-    String str = App.add(input1, input2, input3, input4);
-    assertTrue((str.equals("input1=input2=input3=input4")));
-  }
-  public void testSameArrayList() {
-    ArrayList<Integer> input1 = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1));
-    String str = App.add(input1, input1, input1, input1);
-    assertTrue((str.equals("input1=input2=input3=input4")));
-  }
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+ public void testFound() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      assertTrue(new App().search(array, 4));
+    }
+
+    public void testNotFound() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      assertFalse(new App().search(array, 5));
+    }
+
+    public void testEmptyArray() {
+      ArrayList<Integer> array = new ArrayList<>();
+      assertFalse(new App().search(array, 1));
+    }
+
+    public void testNull() {
+      assertFalse(new App().search(null, 1));
+    }
+
 }
