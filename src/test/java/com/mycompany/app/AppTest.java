327a24abe34559df957ce7b07d0e52038793ae78
mertilovski <mertkaya@etu.edu.tr>
2019-02-02 22:32:35 +0300

    compute.mustache fix

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 import java.util.Map;
 public class AppTest extends TestCase{
 
-  public void testAddMethod(){
+  public void testStandartDeviationMethodMixCases(){
 
     ArrayList<Integer> input1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
-    ArrayList<Integer> input2 = new ArrayList<Integer>(Arrays.asList(1,1,2,4,1));
+    ArrayList<Integer> input2 = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1,1,1,1,1,1));
     ArrayList<Integer> input3 = new ArrayList<Integer>(Arrays.asList(1,1,2,4,3));
     ArrayList<Integer> input4 = new ArrayList<Integer>(Arrays.asList(5,5,5,5,5));
-    String str = App.add(input1, input2, input3, input4);
-    assertTrue((str.equals("input4")));
+    String str = App.standartDeviation(input1, input2, input3, input4);
+    assertTrue((str.equals("2nd list.Its standart deviation is : 1.0")));
 
   }
   public void testWithNull() {
